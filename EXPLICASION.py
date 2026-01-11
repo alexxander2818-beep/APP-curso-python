4789371502b4f1fe6585881b4a83489746a728c4
david martinez <davidmartinez030404@gmail.com>
2026-01-11 18:50:45 -0500

    .

---
+++ EXPLICASION.py
@@ -0,0 +1,12 @@
+#Conceptos Básicos Palabra x Palabra
+
+from
+ 
+#Q es: Es como decir "de dónde" vas a traer algo. Es pa decirle a Python "oye, tráeme esta cosa DE este lugar"
+
+#Ejemplo cotidiano:
+
+from app import create_app
+
+#Es como decir: "de la carpeta 'app', tráeme la función 'create_app'"
+#Sería como decir: "de la tienda tráeme pan" → from tienda import pan
