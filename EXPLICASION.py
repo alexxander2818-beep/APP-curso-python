2b6230a199d28b4a41c3140786f6934000d30d90
david martinez <davidmartinez030404@gmail.com>
2026-01-11 18:57:12 -0500

    .

--- EXPLICASION.py
+++ EXPLICASION.py
@@ -2,11 +2,24 @@
 
 from
  
-#Q es: Es como decir "de dónde" vas a traer algo. Es pa decirle a Python "oye, tráeme esta cosa DE este lugar"
+"""#Q es: Es como decir "de dónde" vas a traer algo. Es pa decirle a Python "oye, tráeme esta cosa DE este lugar"
 
-#Ejemplo cotidiano:
+#Ejemplo cotidiano:"""
 
 from app import create_app
 
-#Es como decir: "de la carpeta 'app', tráeme la función 'create_app'"
-#Sería como decir: "de la tienda tráeme pan" → from tienda import pan
+"""#Es como decir: "de la carpeta 'app', tráeme la función 'create_app'"
+
+#Sería como decir: "de la tienda tráeme pan" → from tienda import pan"""
+
+import
+
+"""#Q es: Es "traer" o "importar". Es como traer una caja de herramientas q alguien más ya hizo pa q tu no tengas q hacerlas d nuevo.
+#Ejemplos:"""
+
+import flask  """tráeme TODO lo de flask"""
+from flask import Flask  """# de flask, solo tráeme Flask"""
+"""#Es como:
+
+#import = tráeme toda la caja de herramientas
+#from X import Y = de esa caja, solo tráeme el martillo"""
